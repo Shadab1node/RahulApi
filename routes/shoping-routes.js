6f6b272f8fc533e7d32280ce18a0c5b48b3849c1
Shadab1node <node1flyweis@gmail.com>
2022-02-09 17:09:20 +0530

    Aaaaaaaaaaaaaaa

--- routes/shoping-routes.js
+++ routes/shoping-routes.js
@@ -4,7 +4,7 @@ const auth=require("../midleware/auth")
 
 // CATEGORY ROUTES
 
-router.route("/addshoping").post(auth.customerloggedIn,Controller.addshoping);
+router.route("/addshoping").post(Controller.addshoping);
 router.route("/getshoping").get(Controller.getshoping);
 router.route("/getshopingbyid/:id").get(Controller.getshopingbyid);
 router.route("/getitembyvender/:id").get(Controller.itemgetbyvenderid)
